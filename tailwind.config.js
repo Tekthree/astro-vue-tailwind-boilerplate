89592ee94d89889808a014f8977f71f4edb1c154
Tek Jones <tekthree@gmail.com>
2022-02-15 14:57:15 -0800

    fixes typescript compatability

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,17 @@
+const colors = require("tailwindcss/colors");
+
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        teal: colors.teal,
+        cyan: colors.cyan,
+      },
+    },
   },
-  plugins: [],
+  plugins: [
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/aspect-ratio"),
+  ],
 };
